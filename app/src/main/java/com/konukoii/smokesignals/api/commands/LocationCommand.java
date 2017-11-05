7eff20da497f1c5295fcb93e78cc927475077e9e
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2017-11-05 09:59:31 -0800

    Added implementation for location command

--- app/src/main/java/com/konukoii/smokesignals/api/commands/LocationCommand.java
+++ app/src/main/java/com/konukoii/smokesignals/api/commands/LocationCommand.java
@@ -1,6 +1,9 @@
 package com.konukoii.smokesignals.api.commands;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+
 import com.konukoii.smokesignals.api.Command;
 
 /**
@@ -10,7 +13,23 @@ import com.konukoii.smokesignals.api.Command;
 public class LocationCommand implements Command {
 
     public String execute(Context context, String[] args) {
+        LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
-        return "";
+        Location lastKnown = null;
+
+        if(manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            lastKnown = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        } else if(manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            lastKnown = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if(lastKnown == null) {
+            return "Location unavailable";
+        }
+
+        return "Latitude: " + lastKnown.getLatitude() + "\n" +
+               "Longitude: " + lastKnown.getLongitude() + "\n" +
+               "https://www.google.com/maps/search/?api=1&query=" +
+                lastKnown.getLatitude() + "," + lastKnown.getLongitude();
     }
 }
